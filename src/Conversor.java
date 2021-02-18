05292100c294b342438d78bb46ecb596f79e5146
RicardoReyes44 <rickyboy1002@gmail.com>
2021-02-18 11:37:43 -0600

    Se agregaron las variables gbc y gbl en la clase 'Conversor'

--- src/Conversor.java
+++ src/Conversor.java
@@ -4,8 +4,11 @@ import java.awt.*;
 
 public class Conversor extends JFrame{
 	
+	GridBagLayout gbl = new GridBagLayout();
+	GridBagConstraints gbc = new GridBagConstraints();
+
+	
 	public Conversor() {
-		
 		
 		
 	}
