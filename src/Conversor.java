bf59503738c4da35b924faa612e7c58c59d0a104
RicardoReyes44 <rickyboy1002@gmail.com>
2021-02-18 12:13:37 -0600

    Se agrego el JLabel 'lbl1' y se alineo

--- src/Conversor.java
+++ src/Conversor.java
@@ -16,11 +16,19 @@ public class Conversor extends JFrame{
 		setVisible(true);
 		setResizable(false);
 		
+		// ------------------------------------------------
+		
+		JLabel lbl1 = new JLabel("Convertir: ");
+		alinear(0, 0, 1, 1, lbl1);
+		
+		// ------------------------------------------------
+		
 		pack();
 		setLocationRelativeTo(null);
 		
 	}
 	
+
 	public void alinear(int x, int y, int width, int height, Component componente) {
 
 		gbc.gridx = x;
