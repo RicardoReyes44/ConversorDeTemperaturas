f7313b5320a6d60753ef41a282139e5fd771e262
RicardoReyes44 <rickyboy1002@gmail.com>
2021-02-18 11:47:12 -0600

    Se agregaron ls utlidades de swing en la clase 'Prueba'

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,9 +1,17 @@
+import javax.swing.SwingUtilities;
+
 
 public class Prueba {
 
 	public static void main(String[] args) {
 		
-		
+		SwingUtilities.invokeLater(new Runnable() {			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				new Conversor();
+			}
+		});
 
 	}
 
